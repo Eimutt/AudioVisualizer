3b8f06f8aa801c64efb99afc043e2180b93b5cec
Simoid <simonkawaji@gmail.com>
2019-04-24 17:27:47 +0200

    Test commit

---
+++ Assets/Scripts/InstantEight.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InstantEight : MonoBehaviour
+{   
+    public GameObject _sampleCubePrefab;
+    GameObject[] _sampleCube = new GameObject[8];
+    public float _maxScale;
+    public float _scaleMult;
+    public float _startScale;
+    public float startPos;
+    public float posStep = 10;
+    public float baseSize;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPos = -40 + posStep/2;
+        for(int i = 0; i < _sampleCube.Length ; i++){
+            GameObject _instanceSampleCube = (GameObject) Instantiate(_sampleCubePrefab);
+            _instanceSampleCube.transform.position = this.transform.position;
+            _instanceSampleCube.transform.parent = this.transform;
+            _instanceSampleCube.name = "SampleCubeMiddle" + "_" + i;
+            
+            _instanceSampleCube.transform.position += new Vector3(startPos + posStep*i,0,0);
+            _sampleCube[i] = _instanceSampleCube;
+        }        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < _sampleCube.Length; i++){
+            if(_sampleCube != null){
+                print(MainAudio._freqBand[i]);
+                _sampleCube[i].transform.localScale = new Vector3(baseSize, Mathf.Min(_maxScale ,MainAudio._freqBand[i] * _scaleMult) + _startScale ,baseSize);
+                _sampleCube[i].transform.localPosition = new Vector3(
+                    _sampleCube[i].transform.localPosition.x, 
+                    Mathf.Min(_maxScale ,MainAudio._freqBand[i] * _scaleMult) /2,
+                    _sampleCube[i].transform.localPosition.z
+                    );
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/MainAudio.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (AudioSource))]
+public class MainAudio : MonoBehaviour
+{
+    AudioSource _audioSource;
+    public static float[] _samples = new float[512];
+    public static float[] _freqBand = new float[8];
+    // Start is called before the first frame update
+    void Start()
+    {
+        _audioSource = GetComponent<AudioSource>();
+        _audioSource.time = _audioSource.clip.length * 0.3f;
+        _audioSource.volume = 1.0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        getSpectrumAudioSource();
+        MakeFreqBands();
+    }
+
+    void getSpectrumAudioSource(){
+        _audioSource.GetSpectrumData(_samples, 0, FFTWindow.Blackman);
+    }
+
+    //Divide the audio to 8 floats
+    //Original hertz is 22050, we have 512 samples.
+    //43 hertz per sample
+    //
+    void MakeFreqBands(){
+        int count = 0;
+        for (int i = 0; i < 8; i++){
+            float sampleAverage = 0;
+            int sampleCount = (int) Mathf.Pow(2,i) * 2;
+            if(i == 7){
+                sampleCount += 2;
+            }
+            for(int j = 0; j < sampleCount; j++){
+                sampleAverage += _samples[count] * (count + 1);
+                count++;
+            }
+
+            sampleAverage /= count;
+
+            _freqBand[i] = sampleAverage*10;
+        }
+    }
+}
